649b83e2a0c74d010ec21266f4d6ff6300ce8e75
Dom Z <91247640+0xdomyz@users.noreply.github.com>
2022-08-16 22:40:36 +1000

    Create postgre_pool.sql

---
+++ postgre_pool.sql
@@ -0,0 +1,8 @@
+SELECT
+    extract(epoch from timestamp '2030-08-15 03:30:45'),
+    timestamp "2030-08-15 03:30:45",
+    TO_TIMESTAMP('2017-03-31 9:30:20', 'YYYY-MM-DD HH:MI:SS'),
+from table;
+
+
+
