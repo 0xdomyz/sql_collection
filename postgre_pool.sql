58235308d6d73ff8af693eb7387a58293534feb6
Dom Z <91247640+0xdomyz@users.noreply.github.com>
2022-09-10 00:46:33 +1000

    Update postgre_pool.sql

--- postgre_pool.sql
+++ postgre_pool.sql
@@ -2,6 +2,7 @@ SELECT
     extract(epoch from timestamp '2030-08-15 03:30:45'),
     timestamp "2030-08-15 03:30:45",
     TO_TIMESTAMP('2017-03-31 9:30:20', 'YYYY-MM-DD HH:MI:SS'),
+    date_trunc('day', now())::date,
 from table;
 
 
