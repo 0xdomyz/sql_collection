21757bbaf695a9b5b2d91549f5ea16f3043a2fac
Dom Z <91247640+0xdomyz@users.noreply.github.com>
2022-10-18 17:15:28 +1100

    Update postgre_pool.sql

--- postgre_pool.sql
+++ postgre_pool.sql
@@ -3,6 +3,8 @@ SELECT
     timestamp "2030-08-15 03:30:45",
     TO_TIMESTAMP('2017-03-31 9:30:20', 'YYYY-MM-DD HH:MI:SS'),
     date_trunc('day', now())::date,
+    start_time,
+    start_time + resolution * interval '1 second' as end_time
 from table
 where time :: date = '2022-08-23'
 ;
