dbf431d72783b944718556d98ca4106e5070936d
Dom Z <91247640+0xdomyz@users.noreply.github.com>
2023-06-06 17:36:36 +1000

    Update code_pool.sql

--- code_pool.sql
+++ code_pool.sql
@@ -155,5 +155,23 @@ load data
     fields terminated by "," optionally enclosed by '"'
     (col1, col2, col3)
 
+--window
 
-
+SELECT empid, ordermonth, qty, 
+  SUM(qty) OVER (PARTITION BY empid 
+                 ORDER BY ordermonth 
+                 ROWS BETWEEN UNBOUNDED PRECEDING 
+                          AND CURRENT ROW) AS run_sum_qty, 
+  AVG(qty) OVER (PARTITION BY empid 
+                 ORDER BY ordermonth 
+                 ROWS BETWEEN UNBOUNDED PRECEDING 
+                          AND CURRENT ROW) AS run_avg_qty, 
+  MIN(qty) OVER (PARTITION BY empid 
+                 ORDER BY ordermonth 
+                 ROWS BETWEEN UNBOUNDED PRECEDING 
+                          AND CURRENT ROW) AS run_min_qty, 
+  MAX(qty) OVER (PARTITION BY empid 
+                 ORDER BY ordermonth 
+                 ROWS BETWEEN UNBOUNDED PRECEDING 
+                          AND CURRENT ROW) AS run_max_qty 
+FROM Sales.EmpOrders;
